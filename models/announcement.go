9762f9a332fbd90e5c79dad9f0af7addf7a3acce
Maulana Hikam <maulanahikam085@gmail.com>
2024-08-24 14:00:22 +0700

    [Models] Handle Custom Time for Published_at

--- models/announcement.go
+++ models/announcement.go
@@ -1,11 +1,39 @@
 package models
 
-import "time"
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
+type CustomTime time.Time
+
+const customTimeFormat = "2006-01-02 15:04:05"
+
+func (ct *CustomTime) UnmarshalJSON(b []byte) error {
+    s := strings.Trim(string(b), "\"")
+    t, err := time.Parse(customTimeFormat, s)
+    if err != nil {
+        return err
+    }
+    *ct = CustomTime(t)
+    return nil
+}
+
+func (ct CustomTime) MarshalJSON() ([]byte, error) {
+    return []byte(fmt.Sprintf("\"%s\"", time.Time(ct).Format(customTimeFormat))), nil
+}
+
+func (ct CustomTime) Format(format string) string {
+    t := time.Time(ct)
+    return t.Format(format)
+}
+
 
 type Announcement struct {
 	ID           uint   `json:"id"`
 	Judul        string `json:"judul"`
 	Penulis      string `json:"penulis"`
 	Konten       string `json:"konten"`
-	Published_at time.Time `json:"published_at"`
+	Published_at CustomTime `json:"published_at"`
 }
